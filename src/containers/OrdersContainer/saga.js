ef9be968f7301e8be4a88be710fc40769fbe9a6b
Maciej Kankowski <maciej.kankowski@jitsolutions.pl>
2018-09-14 09:42:23 +0200

    Refactor saga template

--- src/containers/OrdersContainer/saga.js
+++ src/containers/OrdersContainer/saga.js
@@ -1,11 +1,11 @@
 import { select, takeLatest } from 'redux-saga/effects';
-// import { makeSelectCurrentPage, makeSelectPageSize, makeSelectFilter } from './selectors';
-// import { SEARCH_PEOPLE, receivedPeople, CHANGE_PAGE, HANDLE_REFRESH } from './actions';
+// import { makeSelectOrders } from './selectors';
+// import { getOrdersList } from './actions';
 
 export function* getOrdersList() {
-  const endpoint = 'profiles/search';
-  // const numberOfElements = yield select(makeSelectPageSize());
-  //yield callGet(url, receivedPeople, undefined, options, value => value.json);
+  // const endpoint;
+  // yield select();
+  // yield callGet(url, successAction, undefined, options, value => value.json);
 }
 
 export function* OrdersSaga() {
